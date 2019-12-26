5e7bbefdc992b3d93f6afb7a9da6e8303f42a539
Numbericons <zachjoliver@gmail.com>
2019-12-25 22:43:52 -0800

    Test 4 passed

--- dupeItems.js
+++ dupeItems.js
@@ -1,24 +1,23 @@
 function duplicateCount(text) {
   let count = 0;
   const cntHash = {};
-  let str = text.slice(0);
+  let str = text.slice(0).toLowerCase();
 
   for (let i = 0; i < str.length; i++) {
-    let cntChar = cntHash[str[i]];
-    if (cntChar) {
-      if (cntChar === 1) count++;
-      cntChar += 1;
+    if (cntHash[str[i]]) {
+      if (cntHash[str[i]] === 1) count++;
+      cntHash[str[i]] += 1;
     } else {
       cntHash[str[i]] = 1;
     }
   }
-
+  // console.log(cntHash);
   return count;
 }
-
-// Test.assertEquals(duplicateCount(""), 0);
+// let res = duplicateCount("") //0
 // Test.assertEquals(duplicateCount("abcde"), 0);
 // Test.assertEquals(duplicateCount("aabbcde"), 2);
 // Test.assertEquals(duplicateCount("aabBcde"), 2, "should ignore case");
-// Test.assertEquals(duplicateCount("Indivisibility"), 1)
+let res = duplicateCount("Indivisibility");
+console.log(`Result is ${res} and should be 1`);
 // Test.assertEquals(duplicateCount("Indivisibilities"), 2, "characters may not be adjacent")
